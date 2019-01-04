cb3aee7fc18293cde2dbb99c96d4d7d7aae65cfe
Noah Zoschke <noah@mixable.net>
2019-01-04 08:22:30 -0800

    put headers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
     def search
+        puts request.env.select {|k,v|
+            k.match("^HTTP.*|^CONTENT.*|^REMOTE.*|^REQUEST.*|^AUTHORIZATION.*|^SCRIPT.*|^SERVER.*")
+        }
         render plain: "No results for '#{params[:q]}'"
     end
 end
